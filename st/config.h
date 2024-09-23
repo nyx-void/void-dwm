d0ac7025eff914365bd1c683e31e857db972da61
nyx-void <amrit44404@proton.me>
2024-09-23 14:09:07 +0545

    back to monospace

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack:weight=bold:pixelsize=12:antialias=true:autohint=true";
+static char *font = "monospace:weight=bold:pixelsize=12:antialias=true:autohint=true";
 static char *font2[] = { "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 2;
 
